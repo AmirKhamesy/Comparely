68e2d5837a60cae4a23fe738454162cde47c136f
Amir <iknubs@gmail.com>
2022-06-06 22:13:29 -0700

    WIP: Use refresh token to prevent user re-login and trying to load user playlist

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,41 @@
+
+import React, { useState, useEffect } from 'react';
+import { getPlaylists, token, logout } from './controllers/spotify';
+
 const SPOTIFY_LOGIN_URL = 'http://localhost:8888/login'
 
+
+const logPlaylist = async () => {
+  const data = await getPlaylists()
+  console.log(data)
+}
+
 function App() {
+  const [accessToken, setAccessToken] = useState('');
+
+  useEffect(() => {
+    setAccessToken(token);
+  }, []);
+
+  useEffect(() => {
+    accessToken && accessToken !== '' &&
+      logPlaylist()
+  }, [accessToken]);
+
   return (
     <div >
-      <a
-        href={SPOTIFY_LOGIN_URL}
-      >
-        Login with spotify
-      </a>
+      {accessToken ?
+        <a
+          href={SPOTIFY_LOGIN_URL}
+        >
+          Login with spotify
+        </a>
+        :
+        <>
+          <p>Logged in</p>
+          <button onClick={() => logout()} >Logout</button>
+        </>
+      }
     </div>
   );
 }
