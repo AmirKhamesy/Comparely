4a8f2b24c4c9cfd88e14bb4dfd7031e583d0f01b
Amir <iknubs@gmail.com>
2022-06-13 21:11:26 -0700

    WIP:Adding react router, seperated home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,66 +1,21 @@
 
-import React, { useState, useEffect } from 'react';
-import { getPlaylists, getMorePlaylists, token, logout } from './spotify';
-
-const SPOTIFY_LOGIN_URL = 'http://localhost:8888/login'
-const SPOTIFY_PLAYLIST_PULL_LIMIT = 20;
-
-const getAllPlaylists = async () => {
-  try {
-    const response = await getPlaylists()
-    let { items, next, offset, total } = response.data;
-    let all_playlists = [...items]
-    while (total > (offset + SPOTIFY_PLAYLIST_PULL_LIMIT)) {
-      let morePlaylistsResponse = await getMorePlaylists(next);
-      ({ offset, next } = morePlaylistsResponse.data)
-      all_playlists = all_playlists.concat(morePlaylistsResponse.data.items)
-    }
-    return all_playlists
-  } catch (error) {
-    console.log(error)
-  }
-}
+import React from 'react';
+import Home from './pages/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
-  const [accessToken, setAccessToken] = useState('');
-  const [playlists, setPlaylists] = useState([])
-
-  useEffect(() => {
-    if (accessToken) {
-      const playlists = getAllPlaylists()
-      playlists.then(e => setPlaylists(e))
-    }
-  }, [accessToken])
-
-
-  useEffect(() => {
-    setAccessToken(token);
-
-  }, []);
 
   return (
-    <div >
-      {!accessToken ?
-        <a
-          href={SPOTIFY_LOGIN_URL}
-        >
-          Login with spotify
-        </a>
-        :
-        <>
-          <div style={{ flexDirection: "row", display: "flex", justifyContent: "space-between" }}>
-            <p>Logged in</p>
-            <button onClick={logout} >Logout</button>
-          </div>
-          <div style={{ flexDirection: "column", display: "flex" }}>
-            {
-              playlists &&
-              playlists.map((playlist, idx) => <p key={`playlist-${idx}`}>{playlist.name}</p>)
-            }
-          </div>
-        </>
-      }
-    </div>
+    <Router>
+      <div >
+        <Home></Home>
+      </div>
+    </Router>
   );
 }
 
