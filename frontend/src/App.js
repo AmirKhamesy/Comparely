e2c5b4578c19728b5efaa51081ed0fc3986d260d
Amir <iknubs@gmail.com>
2022-06-18 14:24:45 -0700

    Adding background gradient color

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,11 +7,32 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import { styled } from '@mui/system';
+
+// https://github.com/johnpolacek/animated-gradient-background-generator
+const StyledBackgroundWrapper = styled('div')({
+  background: "linear-gradient(89deg,#1507ab,#f21665)",
+  backgroundSize: "120% 120%",
+  animation: "gradient-animation 10s ease infinite",
+
+  "@keyframes gradient-animation": {
+    '0%': {
+      backgroundPosition: "0% 50%"
+    },
+    "50%": {
+      backgroundPosition: "100% 50%"
+    },
+    "100%": {
+      backgroundPosition: "0% 50%"
+    }
+  }
+})
+
 
 function App() {
 
   return (
-    <div >
+    <StyledBackgroundWrapper >
       {/* https://mui.com/material-ui/getting-started/installation/#font-icons */}
       <link
         rel="stylesheet"
@@ -20,7 +41,7 @@ function App() {
       <Router>
         <Home></Home>
       </Router>
-    </div>
+    </StyledBackgroundWrapper>
   );
 }
 
