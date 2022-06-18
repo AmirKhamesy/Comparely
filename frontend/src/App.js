9dc2a48fdff643ca9bed9105ba7876262083203e
Amir <iknubs@gmail.com>
2022-06-18 14:33:20 -0700

    Moving background gradient color to css files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,32 +7,11 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import { styled } from '@mui/system';
-
-// https://github.com/johnpolacek/animated-gradient-background-generator
-const StyledBackgroundWrapper = styled('div')({
-  background: "linear-gradient(89deg,#1507ab,#f21665)",
-  backgroundSize: "120% 120%",
-  animation: "gradient-animation 10s ease infinite",
-
-  "@keyframes gradient-animation": {
-    '0%': {
-      backgroundPosition: "0% 50%"
-    },
-    "50%": {
-      backgroundPosition: "100% 50%"
-    },
-    "100%": {
-      backgroundPosition: "0% 50%"
-    }
-  }
-})
-
 
 function App() {
 
   return (
-    <StyledBackgroundWrapper >
+    <div >
       {/* https://mui.com/material-ui/getting-started/installation/#font-icons */}
       <link
         rel="stylesheet"
@@ -41,7 +20,7 @@ function App() {
       <Router>
         <Home></Home>
       </Router>
-    </StyledBackgroundWrapper>
+    </div>
   );
 }
 
